90669c958676abe93afb4fe12ef335d149c691f3
MuhammedAlkhayat <your-email@example.com>
2026-04-16 10:39:49 +0300

    fix: AuthContext import path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 
 // Styles
